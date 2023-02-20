508c81315bef92ee4a4e0a4e82ded20a29a22c47
Rafał Borysionek <22373844+MrDarthShoe@users.noreply.github.com>
2023-02-20 12:43:53 +0100

    now validation

--- src/validity/validator.rs
+++ src/validity/validator.rs
@@ -4,30 +4,41 @@ use chrono::{DateTime, Utc};
 use super::Validity;
 
 //TODO Rafał rename validity to constraints
-pub fn validate_validity(parent: &Validity, child: &Validity) -> Result<()> {
+pub fn validate_validity(parent: &Validity, child: &Validity, now: DateTime<Utc>) -> Result<()> {
     validate_not_before(&parent.not_before, &child.not_before)?;
     validate_not_after(&parent.not_after, &child.not_after)?;
+    validate_current_time(child, now)?;
 
     Ok(())
 }
 
-fn validate_not_before(parent: &DateTime<Utc>, child: &DateTime<Utc>) -> Result<()> {
-    if child >= parent {
-        Ok(())
+fn validate_current_time(child: &Validity, now: DateTime<Utc>) -> Result<()> {
+    if now > child.not_after {
+        Err(anyhow!("Child is not valid anymore"))
+    } else if now < child.not_before {
+        Err(anyhow!("Child is not valid yet"))
     } else {
+        Ok(())
+    }
+}
+
+fn validate_not_before(parent: &DateTime<Utc>, child: &DateTime<Utc>) -> Result<()> {
+    if parent > child {
         Err(anyhow!(
             "Child 'not_before' property cannot be earlier than parent one"
         ))
+    } else {
+        Ok(())
     }
 }
 
 fn validate_not_after(parent: &DateTime<Utc>, child: &DateTime<Utc>) -> Result<()> {
-    if child <= parent {
-        Ok(())
-    } else {
+    if child > parent {
         Err(anyhow!(
             "Child 'not_after' property cannot be later than parent one"
         ))
+    } else {
+        Ok(())
     }
 }
 
@@ -47,8 +58,9 @@ mod should {
             not_before: dt("2000-01-01T01:01:01Z"),
             not_after: dt("2000-01-01T03:03:03Z"),
         };
+        let now = dt("2000-01-01T02:02:02Z");
 
-        assert!(validate_validity(parent, child).is_ok());
+        assert!(validate_validity(parent, child, now).is_ok());
     }
 
     #[test]
@@ -61,8 +73,9 @@ mod should {
             not_before: dt("2000-01-01T00:00:00Z"),
             not_after: dt("2000-01-01T04:04:04Z"),
         };
+        let now = dt("2000-01-01T02:02:02Z");
 
-        assert!(validate_validity(parent, child).is_ok());
+        assert!(validate_validity(parent, child, now).is_ok());
     }
 
     #[test]
@@ -75,8 +88,9 @@ mod should {
             not_before: dt("2000-01-01T00:00:00Z"),
             not_after: dt("2000-01-01T03:03:03Z"),
         };
+        let now = dt("2000-01-01T02:02:02Z");
 
-        assert!(validate_validity(parent, child).is_err());
+        assert!(validate_validity(parent, child, now).is_err());
     }
 
     #[test]
@@ -89,8 +103,39 @@ mod should {
             not_before: dt("2000-01-01T01:01:01Z"),
             not_after: dt("2000-01-01T05:05:05Z"),
         };
+        let now = dt("2000-01-01T02:02:02Z");
 
-        assert!(validate_validity(parent, child).is_err());
+        assert!(validate_validity(parent, child, now).is_err());
+    }
+
+    #[test]
+    fn reject_because_child_has_expired() {
+        let parent = &Validity {
+            not_before: dt("2000-01-01T00:00:00Z"),
+            not_after: dt("2000-01-01T04:04:04Z"),
+        };
+        let child = &Validity {
+            not_before: dt("2000-01-01T01:01:01Z"),
+            not_after: dt("2000-01-01T03:03:03Z"),
+        };
+        let now = dt("2000-01-01T04:04:04Z");
+
+        assert!(validate_validity(parent, child, now).is_err());
+    }
+
+    #[test]
+    fn reject_because_child_has_not_yet_valid() {
+        let parent = &Validity {
+            not_before: dt("2000-01-01T00:00:00Z"),
+            not_after: dt("2000-01-01T04:04:04Z"),
+        };
+        let child = &Validity {
+            not_before: dt("2000-01-01T01:01:01Z"),
+            not_after: dt("2000-01-01T03:03:03Z"),
+        };
+        let now = dt("2000-01-01T00:00:00Z");
+
+        assert!(validate_validity(parent, child, now).is_err());
     }
 
     mod utils {
