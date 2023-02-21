f1fd063815a96c3961ab9ebb337e163f6a6f0bd5
Rafał Borysionek <22373844+MrDarthShoe@users.noreply.github.com>
2023-02-21 12:34:47 +0100

    sign_node validation

--- src/key_usage/validator.rs
+++ src/key_usage/validator.rs
@@ -1,8 +1,8 @@
 use anyhow::{anyhow, Result};
 
-use super::KeyUsage;
+use super::{KeyUsage, Usage};
 
-pub fn validate_key_usage(parent: &KeyUsage, child: &KeyUsage) -> Result<()> {
+pub fn validate_certificates_key_usage(parent: &KeyUsage, child: &KeyUsage) -> Result<()> {
     match (parent, child) {
         (KeyUsage::All, _) => Ok(()),
         (KeyUsage::Usages(_), KeyUsage::All) => Err(anyhow!(
@@ -12,7 +12,20 @@ pub fn validate_key_usage(parent: &KeyUsage, child: &KeyUsage) -> Result<()> {
             if child.is_subset(parent) {
                 Ok(())
             } else {
-                Err(anyhow!("Child cannot extend keyusages"))
+                Err(anyhow!("Child cannot extend key usages"))
+            }
+        }
+    }
+}
+
+pub fn validate_sign_node(key_usage: &KeyUsage) -> Result<()> {
+    match key_usage {
+        KeyUsage::All => Ok(()),
+        KeyUsage::Usages(usages) => {
+            if usages.contains(&Usage::SignNode) {
+                Ok(())
+            } else {
+                Err(anyhow!("Key usage does not allow to sign nodes"))
             }
         }
     }
@@ -21,8 +34,6 @@ pub fn validate_key_usage(parent: &KeyUsage, child: &KeyUsage) -> Result<()> {
 #[cfg(test)]
 mod should {
     use super::*;
-
-    use super::super::Usage;
 
     use utils::*;
 
@@ -35,7 +46,7 @@ mod should {
     fn accept_bacause_parent_has_all_permissions(child: KeyUsage) {
         let parent = KeyUsage::All;
 
-        assert!(validate_key_usage(&parent, &child).is_ok());
+        assert!(validate_certificates_key_usage(&parent, &child).is_ok());
     }
 
     #[test_case(KeyUsage::Usages([Usage::SignNode].into()))]
@@ -44,7 +55,7 @@ mod should {
     fn reject_bacause_child_requests_all_usage_and_parent_does_not_have_one(parent: KeyUsage) {
         let child = KeyUsage::All;
 
-        assert!(validate_key_usage(&parent, &child).is_err());
+        assert!(validate_certificates_key_usage(&parent, &child).is_err());
     }
 
     #[test_case(&[], &[])]
@@ -56,7 +67,7 @@ mod should {
         let parent = slice_to_usages(parent);
         let child = slice_to_usages(child);
 
-        assert!(validate_key_usage(&parent, &child).is_ok());
+        assert!(validate_certificates_key_usage(&parent, &child).is_ok());
     }
 
     #[test_case(&[], &[Usage::SignNode])]
@@ -66,9 +77,34 @@ mod should {
         let parent = slice_to_usages(parent);
         let child = slice_to_usages(child);
 
-        assert!(validate_key_usage(&parent, &child).is_err());
+        assert!(validate_certificates_key_usage(&parent, &child).is_err());
     }
 
+    #[test_case(&[Usage::SignNode])]
+    #[test_case(&[Usage::SignNode, Usage::SignManifest])]
+    #[test_case(&[Usage::SignNode, Usage::SignCertificate])]
+    #[test_case(&[Usage::SignNode, Usage::SignCertificate, Usage::SignManifest])]
+    fn accept_sign_node_validation_because_cert_has_proper_usage(key_usage: &[Usage]) {
+        let key_usage = slice_to_usages(key_usage);
+
+        assert!(validate_sign_node(&key_usage).is_ok());
+    }
+
+    #[test]
+    fn accept_sign_node_validation_because_cert_has_all_usage() {
+        let key_usage = KeyUsage::All;
+
+        assert!(validate_sign_node(&key_usage).is_ok());
+    }
+
+    #[test_case(&[])]
+    #[test_case(&[Usage::SignManifest])]
+    #[test_case(&[Usage::SignCertificate, Usage::SignManifest])]
+    fn reject_sign_node_validation_because_cert_has_no_proper_usage(key_usage: &[Usage]) {
+        let key_usage = slice_to_usages(key_usage);
+
+        assert!(validate_sign_node(&key_usage).is_err());
+    }
     mod utils {
         use super::*;
 
