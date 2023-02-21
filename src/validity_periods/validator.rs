24a8afb3b6342b91bd21e98d06089a758e81dbc8
Rafał Borysionek <22373844+MrDarthShoe@users.noreply.github.com>
2023-02-21 14:17:26 +0100

    extract tests to modules

--- src/validity_periods/validator.rs
+++ src/validity_periods/validator.rs
@@ -27,120 +27,128 @@ pub fn validate_timestamp(periods: &ValidityPeriods, ts: DateTime<Utc>) -> Resul
 }
 
 #[cfg(test)]
-mod should {
+mod tests {
     use utils::*;
 
     use super::*;
 
-    #[test]
-    fn accept_because_child_validity_periods_are_subset_of_parent() {
-        let parent = &ValidityPeriods {
-            not_before: dt("2000-01-01T00:00:00Z"),
-            not_after: dt("2000-01-01T04:04:04Z"),
-        };
-        let child = &ValidityPeriods {
-            not_before: dt("2000-01-01T01:01:01Z"),
-            not_after: dt("2000-01-01T03:03:03Z"),
-        };
+    mod validate_validity_periods_should {
+        use super::*;
 
-        assert!(validate_validity_periods(parent, child).is_ok());
+        #[test]
+        pub(crate) fn accept_because_child_periods_are_subset_of_parent() {
+            let parent = &ValidityPeriods {
+                not_before: dt("2000-01-01T00:00:00Z"),
+                not_after: dt("2000-01-01T04:04:04Z"),
+            };
+            let child = &ValidityPeriods {
+                not_before: dt("2000-01-01T01:01:01Z"),
+                not_after: dt("2000-01-01T03:03:03Z"),
+            };
+
+            assert!(validate_validity_periods(parent, child).is_ok());
+        }
+
+        #[test]
+        pub(crate) fn accept_because_child_periods_are_the_same_as_parent() {
+            let parent = &ValidityPeriods {
+                not_before: dt("2000-01-01T00:00:00Z"),
+                not_after: dt("2000-01-01T04:04:04Z"),
+            };
+            let child = &ValidityPeriods {
+                not_before: dt("2000-01-01T00:00:00Z"),
+                not_after: dt("2000-01-01T04:04:04Z"),
+            };
+
+            assert!(validate_validity_periods(parent, child).is_ok());
+        }
+
+        #[test]
+        pub(crate) fn reject_because_child_has_earlier_not_before() {
+            let parent = &ValidityPeriods {
+                not_before: dt("2000-01-01T01:01:01Z"),
+                not_after: dt("2000-01-01T04:04:04Z"),
+            };
+            let child = &ValidityPeriods {
+                not_before: dt("2000-01-01T00:00:00Z"),
+                not_after: dt("2000-01-01T03:03:03Z"),
+            };
+
+            assert!(validate_validity_periods(parent, child).is_err());
+        }
+
+        #[test]
+        pub(crate) fn reject_because_child_has_later_not_after() {
+            let parent = &ValidityPeriods {
+                not_before: dt("2000-01-01T00:00:00Z"),
+                not_after: dt("2000-01-01T04:04:04Z"),
+            };
+            let child = &ValidityPeriods {
+                not_before: dt("2000-01-01T01:01:01Z"),
+                not_after: dt("2000-01-01T05:05:05Z"),
+            };
+
+            assert!(validate_validity_periods(parent, child).is_err());
+        }
     }
 
-    #[test]
-    fn accept_because_child_validity_periods_are_the_same_as_parent() {
-        let parent = &ValidityPeriods {
-            not_before: dt("2000-01-01T00:00:00Z"),
-            not_after: dt("2000-01-01T04:04:04Z"),
-        };
-        let child = &ValidityPeriods {
-            not_before: dt("2000-01-01T00:00:00Z"),
-            not_after: dt("2000-01-01T04:04:04Z"),
-        };
+    mod validate_timestamp_should {
+        use super::*;
 
-        assert!(validate_validity_periods(parent, child).is_ok());
-    }
+        #[test]
+        pub(crate) fn reject_timestamp_because_period_has_expired() {
+            let periods = &ValidityPeriods {
+                not_before: dt("2000-01-01T01:01:01Z"),
+                not_after: dt("2000-01-01T03:03:03Z"),
+            };
+            let now = dt("2000-01-01T04:04:04Z");
 
-    #[test]
-    fn reject_because_child_has_earlier_not_before() {
-        let parent = &ValidityPeriods {
-            not_before: dt("2000-01-01T01:01:01Z"),
-            not_after: dt("2000-01-01T04:04:04Z"),
-        };
-        let child = &ValidityPeriods {
-            not_before: dt("2000-01-01T00:00:00Z"),
-            not_after: dt("2000-01-01T03:03:03Z"),
-        };
+            assert!(validate_timestamp(periods, now).is_err());
+        }
 
-        assert!(validate_validity_periods(parent, child).is_err());
-    }
+        #[test]
+        pub(crate) fn reject_timestamp_because_period_is_not_valid_yet() {
+            let periods = &ValidityPeriods {
+                not_before: dt("2000-01-01T01:01:01Z"),
+                not_after: dt("2000-01-01T03:03:03Z"),
+            };
+            let now = dt("2000-01-01T00:00:00Z");
 
-    #[test]
-    fn reject_because_child_has_later_not_after() {
-        let parent = &ValidityPeriods {
-            not_before: dt("2000-01-01T00:00:00Z"),
-            not_after: dt("2000-01-01T04:04:04Z"),
-        };
-        let child = &ValidityPeriods {
-            not_before: dt("2000-01-01T01:01:01Z"),
-            not_after: dt("2000-01-01T05:05:05Z"),
-        };
+            assert!(validate_timestamp(periods, now).is_err());
+        }
 
-        assert!(validate_validity_periods(parent, child).is_err());
-    }
+        #[test]
+        pub(crate) fn accept_timestamp_because_it_fits_periods() {
+            let periods = &ValidityPeriods {
+                not_before: dt("2000-01-01T01:01:01Z"),
+                not_after: dt("2000-01-01T03:03:03Z"),
+            };
+            let now = dt("2000-01-01T02:02:02Z");
 
-    #[test]
-    fn reject_timestamp_because_period_has_expired() {
-        let periods = &ValidityPeriods {
-            not_before: dt("2000-01-01T01:01:01Z"),
-            not_after: dt("2000-01-01T03:03:03Z"),
-        };
-        let now = dt("2000-01-01T04:04:04Z");
+            assert!(validate_timestamp(periods, now).is_ok());
+        }
 
-        assert!(validate_timestamp(periods, now).is_err());
-    }
+        #[test]
+        pub(crate) fn accept_timestamp_because_it_is_the_same_as_not_before() {
+            let periods = &ValidityPeriods {
+                not_before: dt("2000-01-01T01:01:01Z"),
+                not_after: dt("2000-01-01T03:03:03Z"),
+            };
+            let now = dt("2000-01-01T01:01:01Z");
 
-    #[test]
-    fn reject_timestamp_because_period_is_not_valid_yet() {
-        let periods = &ValidityPeriods {
-            not_before: dt("2000-01-01T01:01:01Z"),
-            not_after: dt("2000-01-01T03:03:03Z"),
-        };
-        let now = dt("2000-01-01T00:00:00Z");
+            assert!(validate_timestamp(periods, now).is_ok());
+        }
 
-        assert!(validate_timestamp(periods, now).is_err());
-    }
+        #[test]
+        pub(crate) fn accept_timestamp_because_it_is_the_same_as_not_after() {
+            let periods = &ValidityPeriods {
+                not_before: dt("2000-01-01T01:01:01Z"),
+                not_after: dt("2000-01-01T03:03:03Z"),
+            };
+            let now = dt("2000-01-01T03:03:03Z");
 
-    #[test]
-    fn accept_timestamp_because_it_fits_periods() {
-        let periods = &ValidityPeriods {
-            not_before: dt("2000-01-01T01:01:01Z"),
-            not_after: dt("2000-01-01T03:03:03Z"),
-        };
-        let now = dt("2000-01-01T02:02:02Z");
-
-        assert!(validate_timestamp(periods, now).is_ok());
-    }
-
-    #[test]
-    fn accept_timestamp_because_it_is_the_same_as_not_before() {
-        let periods = &ValidityPeriods {
-            not_before: dt("2000-01-01T01:01:01Z"),
-            not_after: dt("2000-01-01T03:03:03Z"),
-        };
-        let now = dt("2000-01-01T01:01:01Z");
-
-        assert!(validate_timestamp(periods, now).is_ok());
-    }
-
-    #[test]
-    fn accept_timestamp_because_it_is_the_same_as_not_after() {
-        let periods = &ValidityPeriods {
-            not_before: dt("2000-01-01T01:01:01Z"),
-            not_after: dt("2000-01-01T03:03:03Z"),
-        };
-        let now = dt("2000-01-01T03:03:03Z");
-
-        assert!(validate_timestamp(periods, now).is_ok());
+            assert!(validate_timestamp(periods, now).is_ok());
+        }
     }
 
     mod utils {
