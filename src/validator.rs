8d96f3cfd25bfcecab744a6eff53308ceb7407ac
Rafał Borysionek <22373844+MrDarthShoe@users.noreply.github.com>
2023-02-23 12:37:35 +0100

    add schemas to skeleton

--- src/validator.rs
+++ src/validator.rs
@@ -1,4 +1,4 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 
 use crate::schemas::{
     certificate::Certificate,
@@ -6,15 +6,48 @@ use crate::schemas::{
     signed_envelope::{SignedEnvelope, Signer},
 };
 
-//TODO Rafał Proper return value
-pub fn validate_envelope(data: &str) -> Result<()> {
-    let envelope: SignedEnvelope = serde_json::from_str(data)?;
+//TODO Rafał proper return value
+pub fn validate(data: &str) -> Result<()> {
+    let value: serde_json::Value = serde_json::from_str(data)?;
+    let schema = value["$schema"]
+        .as_str()
+        .ok_or(anyhow!("no schema provided"))?;
 
+    match schema {
+        "https://golem.network/schemas/v0/signed-envelop.schema.json" => {
+            let signed_data_schema = value["signedData"]["$schema"]
+                .as_str()
+                .ok_or(anyhow!("no schema provided"))?;
+
+            match signed_data_schema {
+                "todo-node-permissions-schema" => {
+                    let envelope: SignedEnvelope = serde_json::from_value(value)?;
+                    validate_node_permissions_envelope(envelope)},
+
+                "https://golem.network/schemas/v1/certificate.schema.json" => {
+                    let envelope: SignedEnvelope = serde_json::from_value(value)?;
+                    validate_certificate(&envelope)?;
+                    Ok(())
+                }
+                signed_data_schema => Err(anyhow!(
+                    "Following schema in signed data in envelope is not supported yet: {signed_data_schema}"
+                )),
+            }
+        }
+        "https://golem.network/schemas/v1/certificate.schema.json" => {
+            //TODO Do we want to validate certs without envelope?
+            Ok(())
+        }
+        schema => Err(anyhow!("Following schema is not supported yet: {schema}")),
+    }
+}
+
+fn validate_node_permissions_envelope(envelope: SignedEnvelope) -> Result<()> {
     let node_permissions: NodePermissions = serde_json::from_value(envelope.signed_data)?;
 
     for signature in &envelope.signatures {
         match &signature.signer {
-            Signer::SelfSigned => panic!("NODE JSON CANNOT BE SELF SIGNED"),
+            Signer::SelfSigned => return Err(anyhow!("Node Permissions cannot be self-signed")),
             Signer::Certificate(cert_envelope) => {
                 let leaf = validate_certificate(&cert_envelope)?;
 
