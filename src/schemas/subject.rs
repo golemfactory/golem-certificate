c32ef932b130d00173affce2904b7986febf83c9
Przemyslaw Walski <pwalski@users.noreply.github.com>
2023-03-22 12:41:48 +0100

    legalEntity in subject

--- src/schemas/subject.rs
+++ src/schemas/subject.rs
@@ -5,11 +5,11 @@ use serde::{Deserialize, Serialize};
 pub struct Subject {
     pub display_name: String,
     pub contact: Contact,
+    pub legal_entity: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct Contact {
     pub email: String,
-    pub legal_entity: Option<String>,
 }
